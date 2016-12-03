cc6cff82953db89b55820092e34ba1b379aa1824
Jordan Ide <jorrdandied@hotmail.com>
2016-12-03 21:33:50 +0000

    oldschool methods

--- src/app/util.ts
+++
@@ -1,7 +0,0 @@
-export function randomRange(low, high) {
-  return Math.random() * (high - low) + low;
-}
-
-export function rotation(mag, rad) {
-  return [Math.cos(rad) * mag, Math.sin(rad) * mag];
-}
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import { Main } from './app/index';
-window.onload = () => {
-  const main = new Main();
-};

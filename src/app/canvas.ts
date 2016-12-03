9e12d249dbeaa4e7c70132f5f95019ead7f44989
Thom <thom@layerone.io>
2016-12-03 14:53:13 -0500

    Adding base Image

--- src/app/canvas.ts
+++ src/app/canvas.ts
@@ -1,4 +1,4 @@
-import { Container, autoDetectRenderer } from 'pixi.js';
+import { Container, autoDetectRenderer, Texture, Sprite } from 'pixi.js';
 
 export class Canvas {
   stage: any;
@@ -8,7 +8,45 @@ export class Canvas {
   }
   create() {
     this.stage = new Container();
-    this.renderer = autoDetectRenderer(256, 256);
+    this.renderer = autoDetectRenderer(1000, 1000, {backgroundColor : 0x1099bb});
     document.body.appendChild(this.renderer.view);
   }
+
+  renderScene() {
+    this.renderer.render(this.stage);
+  }
+
+  addImage() {
+    console.log('adding');
+    const backgroundTexture = './assets/background.jpg';
+    const foregroundTexture = './assets/foreground.png';
+
+
+
+    const bottomChest = Sprite.fromImage(foregroundTexture);
+    const topChest = Sprite.fromImage(backgroundTexture);
+
+    bottomChest.anchor.x = 0.5;
+    bottomChest.anchor.y = 0.5;
+
+    bottomChest.position.x = 10;
+    bottomChest.position.y = 10;
+    bottomChest.height = 417;
+    bottomChest.width = 394;
+
+    topChest.anchor.x = 0.5;
+    topChest.anchor.y = 0.5;
+
+    topChest.position.x = 10;
+    topChest.position.y = 10;
+    topChest.height = 417;
+    bottomChest.width = 394;
+
+    this.stage.addChild(bottomChest);
+    this.stage.addChild(topChest);
+
+    console.log(this.stage);
+    
+
+  }  
 }
--- src/app/index.ts
+++ src/app/index.ts
@@ -4,5 +4,10 @@ export class Main {
   canvas = new Canvas();
   constructor() {
     this.canvas.create();
+    this.canvas.addImage();
+
+
+   
+    this.canvas.renderScene();
   }
 }
